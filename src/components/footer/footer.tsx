aa6abe788070a4207fab1889cc9336cfa95f4fe2
Oleg Hachaturoff <oleg.hachaturoff@mail.ru>
2023-08-10 20:44:29 +0300

    add foter and options

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h1>Низ</h1>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/option/option.tsx
@@ -0,0 +1,12 @@
+function Option() {
+    return (
+        <div className="option">
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Ваш город</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Введите ваш город"></input>
+            </div>
+        </div>
+    );
+}
+
+export default Option;
